6496e4e8c42d785151aa9d6a1d9e4d238d239e0c
wichlacz <wichlacz@cs.uni-saarland.de>
2020-04-03 13:20:11 +0200

    fixed plan translation for higher level actions

--- src/main/java/de/saar/minecraft/simplearchitect/SimpleArchitect.java
+++ src/main/java/de/saar/minecraft/simplearchitect/SimpleArchitect.java
@@ -3,6 +3,8 @@ package de.saar.minecraft.simplearchitect;
 import com.google.common.collect.Iterables;
 import de.saar.coli.minecraft.MinecraftRealizer;
 import de.saar.coli.minecraft.relationextractor.Block;
+import de.saar.coli.minecraft.relationextractor.Row;
+import de.saar.coli.minecraft.relationextractor.Wall;
 import de.saar.coli.minecraft.relationextractor.MinecraftObject;
 import de.saar.coli.minecraft.relationextractor.Relation;
 import de.saar.coli.minecraft.relationextractor.Relation.Orientation;
@@ -75,8 +77,8 @@ public class SimpleArchitect extends AbstractArchitect {
 
     private static String getResourceAsString(String resName) {
         return new BufferedReader(new InputStreamReader(getResourceStream(resName)))
-            .lines()
-            .collect(Collectors.joining("\n"));
+                .lines()
+                .collect(Collectors.joining("\n"));
     }
 
     private static CostFunction.InstructionLevel getInstructionLevel() {
@@ -88,16 +90,12 @@ public class SimpleArchitect extends AbstractArchitect {
         int mctsruns = 10000; //number of runs the planner tries to do
         int timeout = 10000; //time the planner runs in ms
 
-        var initialworld = getResourceStream("/de/saar/minecraft/worlds/"+scenario+".csv");
-        var domain = getResourceStream("/de/saar/minecraft/domains/"+scenario+"-block.lisp");
-        // String bridgeFancy = "build-bridge 3 66 1 4 3 3";
-        //String bridgeSimple= "build-bridge 3 66 1 3 5 2";
-        // for simple bridge height is height of banister, for bridge fancy height of arch
-        // String house = "build-house x y z width length height"
-        String problem = getResourceAsString("/de/saar/minecraft/domains/"+scenario+"-block.init").strip();
+        var initialworld = getResourceStream("/de/saar/minecraft/worlds/" + scenario + ".csv");
+        var domain = getResourceStream("/de/saar/minecraft/domains/" + scenario + "-block.lisp");
+        String problem = getResourceAsString("/de/saar/minecraft/domains/" + scenario + "-block.init").strip();
         CostFunction.InstructionLevel level = getInstructionLevel();
-        return planner.nlgSearch(mctsruns,timeout, initialworld, problem, domain, level);
-    }  
+        return planner.nlgSearch(mctsruns, timeout, initialworld, problem, domain, level);
+    }
 
     @Override
     public void initialize(WorldSelectMessage message) {
@@ -121,26 +119,87 @@ public class SimpleArchitect extends AbstractArchitect {
         super.setMessageChannel(messageChannel);
     }
 
-    public List<MinecraftObject> transformPlan(JSPlan jshopPlan){
+    public List<MinecraftObject> transformPlan(JSPlan jshopPlan) {
         var result = new ArrayList<MinecraftObject>();
         JSTaskAtom t;
         for (short i = 0; i < jshopPlan.size(); i++) {
             t = (JSTaskAtom) jshopPlan.elementAt(i);
             String task = t.toStr().toString();
             String[] taskArray = task.split(" ");
-            int x = (int) Double.parseDouble(taskArray[1]);
-            int y = (int) Double.parseDouble(taskArray[2]);
-            int z = (int) Double.parseDouble(taskArray[3]);
-            result.add(new Block(x,y,z));
+            int x1, y1, z1, x2, y2, z2, length, height, dir;
+            switch (taskArray[0]) {
+                case "(!place-block":
+                    x1 = (int) Double.parseDouble(taskArray[2]);
+                    y1 = (int) Double.parseDouble(taskArray[3]);
+                    z1 = (int) Double.parseDouble(taskArray[4]);
+                    result.add(new Block(x1, y1, z1));
+                    break;
+                case "(!build-row":
+                    x1 = (int) Double.parseDouble(taskArray[1]);
+                    y1 = (int) Double.parseDouble(taskArray[2]);
+                    z1 = (int) Double.parseDouble(taskArray[3]);
+                    length = (int) Double.parseDouble(taskArray[4]);
+                    dir = (int) Double.parseDouble(taskArray[5]);
+                    //east=1=>x+, west=2=>x-, north=3=>y-, south=4=>y+
+                    if (dir == 1) {
+                        x2 = x1 + length - 1;
+                        z2 = z1;
+                    } else if (dir == 2) {
+                        x2 = x1 - length + 1;
+                        z2 = z1;
+                    } else if (dir == 3) {
+                        x2 = x1;
+                        z2 = z1 - length + 1;
+                    } else if(dir == 4) {
+                        x2 = x1;
+                        z2 = z1 + length - 1;
+                    } else {
+                        System.err.println("Unkown direction in plan transformation");
+                        break;
+                    }
+                    result.add(new Row("row", x1, z1, x2, z2, y1));
+                    break;
+                case "(!build-wall":
+                    x1 = (int) Double.parseDouble(taskArray[1]);
+                    y1 = (int) Double.parseDouble(taskArray[2]);
+                    z1 = (int) Double.parseDouble(taskArray[3]);
+                    length = (int) Double.parseDouble(taskArray[4]);
+                    height = (int) Double.parseDouble(taskArray[5]);
+                    dir = (int) Double.parseDouble(taskArray[6]);
+                    //east=1=>x+, west=2=>x-, north=3=>y-, south=4=>y+
+
+                    if (dir == 1) {
+                        x2 = x1 + length - 1;
+                        z2 = z1;
+                    } else if (dir == 2) {
+                        x2 = x1 - length + 1;
+                        z2 = z1;
+                    } else if (dir == 3) {
+                        x2 = x1;
+                        z2 = z1 - length + 1;
+                    } else if(dir == 4) {
+                        x2 = x1;
+                        z2 = z1 + length - 1;
+                    } else {
+                        System.err.println("Unkown direction in plan transformation");
+                        break;
+                    }
+                    y2 = y1 + height -1;
+                    result.add(new Wall("row", x1, y1, z1, x2, y2, z2));
+                    break;
+                default:
+                    System.out.println("New Action "+ task);
+                    break;
+            }
         }
         return result;
     }
 
-    public HashSet<MinecraftObject> transformState(JSState state){
+    public HashSet<MinecraftObject> transformState(JSState state) {
         HashSet<MinecraftObject> set = new HashSet();
-        for(JSPredicateForm term : state.atoms()){
+        for (JSPredicateForm term : state.atoms()) {
             String name = (String) term.elementAt(0);
-            if(!name.equals("block-at")){
+            if (!name.equals("block-at")) {
                 //System.out.println("Not a block: " + term.toString());
                 continue;
             }
@@ -149,9 +208,11 @@ public class SimpleArchitect extends AbstractArchitect {
             JSTerm tmp = (JSTerm) term.elementAt(2);
             int x = (int) Double.parseDouble(tmp.toStr().toString());
             tmp = (JSTerm) term.elementAt(3);
-            int y = (int) Double.parseDouble(tmp.toStr().toString());;
+            int y = (int) Double.parseDouble(tmp.toStr().toString());
+            ;
             tmp = (JSTerm) term.elementAt(4);
-            int z = (int) Double.parseDouble(tmp.toStr().toString());;
+            int z = (int) Double.parseDouble(tmp.toStr().toString());
+            ;
             //System.out.println("Block: " + type + " " + x + " " + y + " "+ z);
             set.add(new UniqueBlock(type, x, y, z));
         }
@@ -172,7 +233,7 @@ public class SimpleArchitect extends AbstractArchitect {
             int x = request.getX();
             int y = request.getY();
             int z = request.getZ();
-            var blockPlaced = new Block(x,y,z);
+            var blockPlaced = new Block(x, y, z);
             String response = "";
             if (plan.isEmpty()) {
                 sendMessage("you are done, no more changes needed!");
@@ -209,7 +270,7 @@ public class SimpleArchitect extends AbstractArchitect {
                 alreadyPlacedBlocks.add(blockPlaced);
                 it = Set.of(blockPlaced);
             } else {
-                    sendMessage("Not there! please remove that block again and " + currentInstruction);
+                sendMessage("Not there! please remove that block again and " + currentInstruction);
             }
         }
     }
@@ -220,7 +281,7 @@ public class SimpleArchitect extends AbstractArchitect {
         int x = request.getX();
         int y = request.getY();
         int z = request.getZ();
-        var block = new Block(x,y,z);
+        var block = new Block(x, y, z);
         // If a block that should be placed is removed again, re-add it to the plan
         // and instruct the user to place this block again
         if (alreadyPlacedBlocks.contains(block)) {
@@ -242,14 +303,14 @@ public class SimpleArchitect extends AbstractArchitect {
         Orientation newOrientation;
         if (abs(x) > abs(z)) {
             // User looks along X-axis
-            if (x>0) {
+            if (x > 0) {
                 newOrientation = Orientation.XPLUS;
             } else {
                 newOrientation = Orientation.XMINUS;
             }
         } else {
             // looking along Z axis
-            if (z>0) {
+            if (z > 0) {
                 newOrientation = Orientation.ZPLUS;
             } else {
                 newOrientation = Orientation.ZMINUS;
@@ -284,7 +345,7 @@ public class SimpleArchitect extends AbstractArchitect {
                 ),
                 lastOrientation
         );
-        for (var elem: it) {
+        for (var elem : it) {
             relations.add(new Relation("it", elem));
         }
         try {
